f2819142c7c8f913be456e84583e603cfd0ab7de
cirine <1410@holbertonschool.com>
2020-12-10 21:39:05 +0100

    condition for box 0 added

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,7 +8,7 @@ def canUnlockAll(boxes):
     unlocked_boxes = [0]
     for box in range(len(boxes)):
         for key in boxes[box]:
-            if key not in unlocked_boxes and key != box:
+            if key not in unlocked_boxes and key != box and key < len(boxes):
                 unlocked_boxes.append(key)
     if len(unlocked_boxes) == len(boxes):
         return True
