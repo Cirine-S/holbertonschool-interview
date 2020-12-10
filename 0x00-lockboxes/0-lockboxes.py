0ce34e3bb618d5890ebd0b1f8e68512b93a2555b
cirine <1410@holbertonschool.com>
2020-12-10 21:32:20 +0100

    check if all boxes can be unlocked

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+
+    unlocked_boxes = [0]
+    for box in range(len(boxes)):
+        for key in boxes[box]:
+            if key not in unlocked_boxes and key != box:
+                unlocked_boxes.append(key)
+    if len(unlocked_boxes) == len(boxes):
+        return True
+    return False
