75bf74fc86f39fa7fed29f9a290a3e0ed2c26f83
cirine <1410@holbertonschool.com>
2020-12-23 22:50:32 +0100

    max heap

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,16 +1,104 @@
 #include "binary_trees.h"
+/**
+ * swapNode - swap Node.
+ * @myNode: pointer to the first node of the list
+ * Return: void
+ */
+void swapNode(heap_t **myNode)
+{
+	heap_t *move;
+	int tmp;
+
+	for (move = *myNode; move->parent; move = move->parent)
+		if (move->n > move->parent->n)
+		{
+			tmp = move->parent->n;
+			move->parent->n = move->n;
+			move->n = tmp;
+			*myNode = (*myNode)->parent;
+		}
+}
+
+/**
+ * heapify - find the parent node where to insert.
+ * @root: root of the tree
+ * Return: Pointer to the parent node.
+ */
+heap_t *heapify(heap_t **root)
+{
+	heap_t *node = NULL;
+	heap_t *array[100];
+	int front, reer;
+
+	front = reer = 0;
+	array[reer] = *root;
+
+	while (2021)
+	{
+		node = array[front];
+		if (front == reer)
+			front = reer = -1;
+		else
+			front++;
+		if (!node->left)
+		{
+			front = reer = -1;
+			return (node);
+		}
+		else if (!node->right)
+		{
+			front = reer = -1;
+			return (node);
+		}
+		else
+		{
+			reer = reer + 1;
+			array[reer] = node->left;
+			reer = reer + 1;
+			array[reer] = node->right;
+		}
+	}
+	return (node);
+}
+/**
+ * heap_insert - Max HEAP insertion function.
+ * @root: pointer to the root node of the tree
+ * @value: number to be inserted
+ * Return: Pointer to the new created node.
+ */
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-    binary_tree_t *child = malloc(sizeof(binary_tree_t));
-    if (child == NULL)
-        return NULL;
-    
-    child->parent = parent;
-    child->n = value;
-    child->right = NULL;
-    child->left = NULL;
-    return child;
-    
-    return (*root);
+	heap_t *newNode = NULL;
+	heap_t *parentNode = NULL;
+
+	if (*root == NULL)
+	{
+	*root = malloc(sizeof(heap_t));
+    if (*root == NULL)
+		return (NULL);
+	(*root)->n = value;
+	return (*root);
+	}
+
+	parentNode = heapify(root);
+
+	newNode = malloc(sizeof(heap_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->n = value;
+	newNode->parent = parentNode;
+	if (!parentNode->left)
+		parentNode->left = newNode;
+	else
+		parentNode->right = newNode;
+
+	if (newNode->n > parentNode->n)
+	{
+		swapNode(&newNode);
+	}
+
+	return (newNode);
 }
