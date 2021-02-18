a09f9c932bb0971c70ef54f03462c3cbeac47ad7
cirine <1410@holbertonschool.com>
2021-02-18 23:29:06 +0100

    pep

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -30,14 +30,14 @@ def validUTF8(data):
         if binList[i].startswith('0'):
             i += 1
         else:
-            if binList[i].startswith('110'):
+            if binList[i][:3].startswith('110'):
                 tmp = binList[i + 1:i + 2]
                 if (len(tmp) != 1):
                     return False
                 if not (binList[i + 1].startswith('10')):
                     return False
                 i += 2
-            elif binList[i].startswith('1110'):
+            elif binList[i][:4].startswith('1110'):
                 tmp = binList[i + 1:i + 3]
                 if (len(tmp) != 2):
                     return False
@@ -45,7 +45,7 @@ def validUTF8(data):
                         binList[i + 2].startswith('10')):
                     return False
                 i += 3
-            elif binList[i].startswith('11110'):
+            elif binList[i][:5].startswith('11110'):
                 tmp = binList[i + 1:i + 4]
                 if not (len(tmp) == 3):
                     return False
