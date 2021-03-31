82244f318094e2b62c7e6065eb344153f6c65570
cirine <1410@holbertonschool.com>
2021-04-01 00:17:50 +0100

    express lane

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -3,12 +3,13 @@
 
 #include "search.h"
 
-
 /**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: list is a pointer to the head of the skip list to search in
- * @value: is the value to search for
- * Return: a pointer on the 1st node where value is located or NULL on failure
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ *
+ * @list: a pointer to the head of the skiplist to search in.
+ * @value: the value to search for.
+ *
+ * Return: a pointer on the first node where value is located. NULL otherwise.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -17,10 +18,10 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!list)
 		return (NULL);
 	start = list;
-	end = list->express;
+	end = start->express;
 	while (end)
 	{
-		if (value <= end->n || !end->next)
+		if (value <= end->n || end->next != NULL)
 			printf("Value checked at index [%lu] = [%d]\n",
 			       end->index, end->n);
 		if ((start->n <= value && value <= end->n) || end->next == NULL)
