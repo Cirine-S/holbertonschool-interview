7437d96cb804cc4f87b40d53c282472f970f6c21
cirine <1410@holbertonschool.com>
2021-04-11 17:00:18 +0100

    ordered characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  let characters = JSON.parse(body).characters
+  for (let i in characters) {
+	  await new Promise((resolve, reject) => {
+		request(characters[i], (err, response, body) => {
+			console.log(JSON.parse(body).name)
+		resolve()
+	  })
+	})
+}
+})
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,5 @@
+# Starwar charachters
+
+## About
+
+Javascript program that prints all characters of a Star Wars movie, from Star Wars API
