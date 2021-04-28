38ee8f265a4b9b56cd7c42aadd41344dfc2644d8
cirine <1410@holbertonschool.com>
2021-04-28 17:17:38 +0100

    print fix

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -14,14 +14,12 @@ void print_array(int *array, size_t l, size_t r)
 {
 
 	printf("Searching in array: ");
-	for (size_t i = l; i <= r; i++)
+	for (size_t i = l; i < r; i++)
 	{
 		printf("%d", array[i]);
-		if (i != r)
-			printf(", ");
-		else
-			printf("\n");
+		printf(", ");
 	}
+	printf("\n");
 }
 
 /**
