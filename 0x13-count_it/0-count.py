c4b5c2d7cd1dc3f9add3c31334791fd74341542c
cirine <1410@holbertonschool.com>
2021-05-09 18:55:06 +0100

    requesting reddit api

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains a recursive function that queries the Reddit API,
+parses the title of all hot articles, and prints a sorted count of
+given keywords (case-insensitive, delimited by spaces. Javascript should
+count as javascript, but java should not).
+"""
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    a recursive function that queries the Reddit API,
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords
+    """
+    r = requests.get('https://www.reddit.com/r/{}/hot/.json'.format(subreddit))
