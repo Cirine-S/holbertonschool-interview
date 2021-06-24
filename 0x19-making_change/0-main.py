8135ff4c2994739bf0b78c2580badac27b0895c0
cirine <1410@holbertonschool.com>
2021-06-24 16:58:48 +0100

    1st cond forgotten

--- 0x19-making_change/0-main.py
+++ 0x19-making_change/0-main.py
@@ -5,6 +5,6 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([2, 25, 1], 37))
+print(makeChange([2, 25, 1], 0))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
