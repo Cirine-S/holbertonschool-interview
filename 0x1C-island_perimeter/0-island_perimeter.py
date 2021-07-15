00fc8a98863cda6028dea8f37d2b5d332a876025
cirine <1410@holbertonschool.com>
2021-07-15 14:46:38 +0100

    island per

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -16,14 +16,15 @@ def island_perimeter(grid):
     island_per = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if (grid[i][j] == 1):
-                left = grid[i][j + 1]
-                right = grid[i][j - 1]
-                above = grid[i - 1][j]
-                below = grid[i + 1][j]
-                if (left + right + above + below == 2):
-                    island_per += 2
-                if (left + right + above + below == 1):
-                    island_per += 3
+            if (grid[i][j]):
+                if (grid[i][j] == 1):
+                    left = grid[i][j + 1]
+                    right = grid[i][j - 1]
+                    above = grid[i - 1][j]
+                    below = grid[i + 1][j]
+                    if (left + right + above + below == 2):
+                        island_per += 2
+                    if (left + right + above + below == 1):
+                        island_per += 3
 
     return island_per
