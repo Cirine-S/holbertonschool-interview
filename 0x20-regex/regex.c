21cbf0530577749df4f2019c7ab09cb89495ad65
cirine <1410@holbertonschool.com>
2021-08-12 18:06:13 +0100

    regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -12,25 +12,25 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-if (!str || !pattern)
-return (0);
-if (*str == '\0' && *pattern == '\0')
-return (1);
-if (*str == *pattern || (*pattern == '.' && *(pattern + 1) != '*'))
-return (regex_match(++str, ++pattern));
-if (*str != *pattern && (*pattern != '.' && *(pattern + 1) == '*'))
-return (regex_match(str, pattern + 2));
-if (*pattern == '.' && *(pattern + 1) == '*')
-{
-if (!pattern[2])
-return (1);
-return (regex_match(str, pattern + 2) || regex_match(str + 1, pattern));
-}
-if (*pattern == '*')
-{
-if (*(pattern - 1) == *str)
-return (regex_match(str + 1, pattern));
-return (regex_match(str, pattern + 1));
-}
-return (0);
+	if (!str || !pattern)
+		return (0);
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+	if (*str == *pattern || (*pattern == '.' && *(pattern + 1) != '*'))
+		return (regex_match(++str, ++pattern));
+	if (*str != *pattern && (*pattern != '.' && *(pattern + 1) == '*'))
+		return (regex_match(str, pattern + 2));
+	if (*pattern == '.' && *(pattern + 1) == '*')
+	{
+		if (!pattern[2])
+			return (1);
+		return (regex_match(str, pattern + 2) || regex_match(str + 1, pattern));
+	}
+	if (*pattern == '*')
+	{
+		if (*(pattern - 1) == *str)
+			return (regex_match(str + 1, pattern));
+		return (regex_match(str, pattern + 1));
+	}
+	return (0);
 }
--- 0x20-regex/regex.h
+++ 0x20-regex/regex.h
@@ -1,12 +1,6 @@
-#ifndef _REGEX_
-#define _REGEX_
-
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
+#ifndef REGEX
+#define REGEX
 
 int regex_match(char const *str, char const *pattern);
 
-#endif /* _REGEX_ */
+#endif
