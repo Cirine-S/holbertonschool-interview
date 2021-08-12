6a90c6b32b7195c5061b9c331b2f57b958d29cd3
cirine <1410@holbertonschool.com>
2021-08-12 17:56:18 +0100

    regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -6,8 +6,9 @@
 
 /**
  * regex_match - checks whether a given pattern matches a given string
- * @str: str
- * @pattern: pattern
+ * @str: is the string to scan
+ * @pattern: is the regular expression
+ *
  * Return: 1 if the pattern matches the string, or 0 if it doesn’t
  */
 int regex_match(char const *str, char const *pattern)
