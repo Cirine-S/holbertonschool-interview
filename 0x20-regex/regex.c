8244ea6e7bd5ffeb76e0ea39c65c5ad2fa9dd29e
cirine <1410@holbertonschool.com>
2021-08-12 17:51:39 +0100

    regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -6,31 +6,37 @@
 
 /**
  * regex_match - checks whether a given pattern matches a given string
- *
+ * @str: str
+ * @pattern: pattern
  * Return: 1 if the pattern matches the string, or 0 if it doesn’t
  */
 int regex_match(char const *str, char const *pattern)
 {
+	int i = 0, j = 0;
 	(void)pattern;
-	for(int i = 0; str[i]; i++)
+	for (int i = 0; str[i]; i++)
 	{
 		if (str[i] == '.' || str[i] == '*')
 			return (0);
 	}
-	int i = 0, j = 0;
 
-	while (str[i] && pattern[j]) {
+	i = 0;
+	while (str[i] && pattern[j])
+	{
 		if (pattern[j] == '.' && pattern[j + 1] == '*' && !pattern[j + 2])
 		{
 			printf("here");
 			return (1);
 		}
-		else if (str[i] == pattern[j] || pattern[i] == '.') {
+		else if (str[i] == pattern[j] || pattern[i] == '.')
+		{
 			i++;
 			j++;
 		}
-		else if (pattern[j] == '*' && str[i - 1] == pattern[j - 1]) {
-				while (str[i] == str[i - 1]) {
+		else if (pattern[j] == '*' && str[i - 1] == pattern[j - 1])
+		{
+				while (str[i] == str[i - 1])
+				{
 				i++;
 			}
 		}
