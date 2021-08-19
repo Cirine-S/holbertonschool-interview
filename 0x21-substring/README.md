dec0db8b013e6131114c424bbc6e6dd91b11e2a0
cirine <1410@holbertonschool.com>
2021-08-19 20:18:55 +0100

    substr

---
+++ 0x21-substring/README.md
@@ -0,0 +1,3 @@
+# Substring
+
+A program that finds all the possible substrings containing a list of words, within a given string.
---
+++ 0x21-substring/substring.h
@@ -0,0 +1,6 @@
+#ifndef SUBSTRING
+#define SUBSTRING
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
