ff7344ce6dcf91bf37f2b576f779d8f6ec470df1
cirine <1410@holbertonschool.com>
2021-08-26 23:53:40 +0100

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 
+
 def isprime(j):
-    for i in range(2,j):
-        if j%i ==0:
+    """[checks if a number is a prime one]
+
+    Args:
+        j ([int]): [number to check]
+
+    Returns:
+        [boolean]: [true if prime, else false]
+    """
+    for i in range(2, j):
+        if j % i == 0:
             return False
     return True
 
+
 def isWinner(x, nums):
+    """[Prime Game]
+
+    Args:
+        x ([int]): [number of rounds]
+        nums ([list]): [array of n]
+
+    Returns:
+        [string]: [name of the winner]
+    """
     ben = 0
     maria = 0
     for n in nums:
-        if n==1:
+        if n == 1:
             ben += 1
             print("Ben")
         else:
@@ -18,7 +37,7 @@ def isWinner(x, nums):
             for i in range(2, n + 1):
                 if isprime(i):
                     print(i)
-                    count+= 1
+                    count += 1
             if (count % 2 == 0):
                 print("Ben")
                 ben += 1
