10d07217d1ad0b380863a5ab65d5905f9009c0a1
cirine <1410@holbertonschool.com>
2021-08-28 13:53:48 +0100

    del forgotten prints

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -33,19 +33,15 @@ def isWinner(x, nums):
     for n in nums:
         if n == 1:
             ben += 1
-            print("Ben")
         else:
             count = 0
             for i in range(2, n + 1):
                 if isprime(i):
-                    print(i)
                     count += 1
             if (count % 2 == 0):
-                print("Ben")
                 ben += 1
             else:
                 maria += 1
-                print("Maria")
     if (ben > maria):
         return "Ben"
     elif (ben < maria):
--- 0x22-primegame/main_0.py
+++ 0x22-primegame/main_0.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""
+Main file for testing
+"""
 
 isWinner = __import__('0-prime_game').isWinner
 
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(5, [1, 2, 3, 4, 5])))
